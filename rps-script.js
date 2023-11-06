a9eff453dd541d3fb8d59c7f5778b0e9b334b33f
zeynepa9 <zeynepakyolhpal@gmail.com>
2023-11-06 11:21:57 +0300

    final

--- rps-script.js
+++ rps-script.js
@@ -4,5 +4,10 @@ function getComputerChoices{
 
     let items =  Array("rock","paper","scissors");
 
+    let computerChoice = items[Math.floor(Math.random()*items.length)];
+
+    return computerChoice
 
 }
+
+console.log()
